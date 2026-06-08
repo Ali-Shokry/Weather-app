dba884fc1bb02209a973985bcbf875901c8b2911
Ali-Shokry <alishokrytamim2183@gmail.com>
2026-06-08 19:12:06 +0300

    Some Corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // Weather app using function component and custom hooks and tailwind css in styling (ts,npm vite{new method of react app creation })
 import Form from "./components/FormComp";
 import Weather from "./components/WeatheCompr";
-import { useWeather } from "./hooks/UseWeather";
+import { useWeather } from "./Hooks/UseWeather";
 
 const App = () => {
   const { weatherData, fetchWeather, resetData } = useWeather();
@@ -15,7 +15,7 @@ const App = () => {
   };
   const handleReset = () => {
     resetData();
-  }
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-b from-[#797979] to-[#58b0aa] p-4">
       <div className="w-95 min-h-137.5 bg-[#2C4A47] rounded-sm p-8 shadow-2xl flex flex-col justify-center">
